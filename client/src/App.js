8d50b5cf9615f952007a80854961b63c69075a56
admin <admin@admins-MBP.net>
2021-11-01 21:50:54 -0500

    tested after tranfser

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [BCBit1, setBCBit1] = useState("");
+  const [BCEth1, setBCEth1] = useState("");
+  useEffect(() => {
+    const URL = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD";
+    const USER_TOKEN = "82e5aad3-967e-417c-9c6f-82a897b3a603";
+    const AuthString = "Bearer ".concat(USER_TOKEN);
+    axios
+      .get(URL, { headers: { Authorization: AuthString } })
+      .then((response) => {
+        console.log(response.data);
+        setBCBit1(response.data.last_trade_price);
+      })
+      .catch((error) => {
+        console.log("error " + error);
+      });
+  }, []);
+  useEffect(() => {
+    const URL = "https://api.blockchain.com/v3/exchange/tickers/ETH-USD";
+    const USER_TOKEN = "82e5aad3-967e-417c-9c6f-82a897b3a603";
+    const AuthString = "Bearer ".concat(USER_TOKEN);
+    axios
+      .get(URL, { headers: { Authorization: AuthString } })
+      .then((response) => {
+        console.log(response.data);
+        setBCEth1(response.data.last_trade_price);
+      })
+      .catch((error) => {
+        console.log("error " + error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stock Prices</h1>
+      <p>
+        <button onClick={() => window.location.reload(false)}>
+          Click to refresh!
+        </button>
+        <input type="text" name="price" value={BCBit1}></input>
+      </p>
+      <p>
+        <button onClick={() => window.location.reload(false)}>
+          Click to refresh!
+        </button>
+        <input type="text" name="price" value={BCEth1}></input>
+      </p>
     </div>
   );
 }
