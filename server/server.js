4a89e3720fbe86a54c35a65ebde20bdd1d2e5826
admin <admin@admins-MBP.localdomain>
2021-11-03 21:57:26 -0500

    removed socketio code as extraneous

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-const socket = require("socket.io");
 const bodyParser = require("body-parser");
 
 require("./config/mongoose.config");
@@ -13,23 +12,4 @@ app.use(express.urlencoded({ extended: false }));
 require("./routes/stocks.routes")(app);
 const server = app.listen(8000, () => {
   console.log("Listening at Port 8000");
-});
-
-const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT"],
-    allowedHeaders: ["*"],
-    credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("socket id: " + socket.id);
-
-  socket.on("event_from_client", (data) => {
-    // send a message with "data" to ALL clients EXCEPT for the one that emitted the
-    //     "event_from_client" event
-    socket.broadcast.emit("event_to_all_other_clients", data);
-  });
 });
